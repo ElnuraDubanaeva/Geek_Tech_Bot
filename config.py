68c4b1a0084062d4dedc349e8b535bf7aa2e25b3
ElnuraDubanaeva <elena040203@gmail.com>
2022-12-02 14:08:53 +0600

    hw7'

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from decouple import config
 
 storage = MemoryStorage()
-TOKEN = config('TOKEN')
+TOKEN = "5696177932:AAH-2SFIk5ARH-BU34w1h7flQpxn21BM1w0"
 bot = Bot(TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
 admin = [1927522329, ]
